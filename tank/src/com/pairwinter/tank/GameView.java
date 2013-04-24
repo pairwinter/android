2410d87084b12bf7e7f33924f0227c03e957e1d7
damon <pairwinter@126.com>
2013-04-24 10:16:57 +0800

    update

--- tank/src/com/pairwinter/tank/GameView.java
+++ tank/src/com/pairwinter/tank/GameView.java
@@ -2,10 +2,7 @@ package com.pairwinter.tank;
 
 import android.content.Context;
 import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Paint;
+import android.graphics.*;
 import android.util.AttributeSet;
 import android.view.KeyEvent;
 import android.view.View;
@@ -20,28 +17,35 @@ import static android.graphics.BitmapFactory.*;
  * To change this template use File | Settings | File Templates.
  */
 public class GameView extends View {
-    Bitmap bitmap;
     int x=0,y=0;
     public GameView(Context context) {
         super(context);
-        Resources resources = context.getResources();
-        this.bitmap = BitmapFactory.decodeResource(resources,R.drawable.battlecity);
         setFocusable(true);
     }
-
+    private Bitmap generateBitmap(int id){
+        Resources resources = this.getContext().getResources();
+        return BitmapFactory.decodeResource(resources,id);
+    }
+    private Bitmap tankBitmap(){
+//        return this.generateBitmap(R.drawable.)
+        return null;
+    }
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);    //To change body of overridden methods use File | Settings | File Templates.
         Paint textPaint = new Paint();
-        textPaint.setColor(0xff0000);
-        canvas.drawColor(0xffffff);
-        canvas.drawText("Tank",0,50,textPaint);
-        canvas.drawBitmap(bitmap,50,100,new Paint());
+        textPaint.setAntiAlias(Boolean.TRUE.booleanValue());
+        textPaint.setColor(Color.BLACK);
+        textPaint.setTextSize((float)30);
+
+        canvas.drawColor(Color.WHITE);
+//        canvas.drawText("Tank",0,50,textPaint);
+//        canvas.drawBitmap(this.generateBitmap(R.drawable.battlecity),0,0,new Paint());
+        canvas.drawBitmap(this.generateBitmap(R.drawable.game_operator_pick_cycle),this.getLeft(),this.getBottom()-300,new Paint());
     }
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-
         switch (keyCode){
             case KeyEvent.KEYCODE_DPAD_UP:
                 y -= 10;
